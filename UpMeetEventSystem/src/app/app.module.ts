1e44153a164e3cf65e5bf879b3a2cb97a77f1e1e
nikjk <nik.kuehn@outlook.com>
2023-06-06 17:46:45 -0400

    Started working on Events component and interface.

--- UpMeetEventSystem/src/app/app.module.ts
+++ UpMeetEventSystem/src/app/app.module.ts
@@ -6,11 +6,13 @@ import {HttpClientModule} from '@angular/common/http'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
+import { EventsComponent } from './events/events.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    EventsComponent
   ],
   imports: [
     BrowserModule,
---
+++ UpMeetEventSystem/src/app/events.ts
@@ -0,0 +1,11 @@
+export interface Events {
+    EventName: string;
+    EventDescription: string;
+    CreatedBy: string;
+    CreatedDate: Date;
+    EventStartDate: Date;
+    EventEndDate: Date;
+    EventType: string;
+    EventLocation: string;
+    Price: number;
+}
