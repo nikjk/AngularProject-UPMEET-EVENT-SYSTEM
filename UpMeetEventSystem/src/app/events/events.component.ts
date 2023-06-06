7a183b2daf2afafc2c33494a751ef8b6615ad6b3
nikjk <nik.kuehn@outlook.com>
2023-06-06 17:50:27 -0400

    Update events.component.ts

--- UpMeetEventSystem/src/app/events/events.component.ts
+++ UpMeetEventSystem/src/app/events/events.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-events',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./events.component.css']
 })
 export class EventsComponent {
+
+  constructor(private apiService: ApiService){}
+
+  ngOnInit(): void {
+    this.getEvents();
+  }
 
 }
