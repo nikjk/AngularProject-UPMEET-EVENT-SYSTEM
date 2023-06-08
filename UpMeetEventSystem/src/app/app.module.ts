b612d39fc9bb4519d74cfbb8bce0e1ec9455bf28
Jeff Santucci <jeffsantucci@rocketmortgage.com>
2023-06-07 20:54:28 -0400

    event-form

--- UpMeetEventSystem/src/app/app.module.ts
+++ UpMeetEventSystem/src/app/app.module.ts
@@ -7,20 +7,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersComponent } from './users/users.component';
 import { EventsComponent } from './events/events.component';
-import { FavoritesComponent } from './favorites/favorites.component';
-import { FormsModule } from '@angular/forms';
+import { FormComponent } from './event-form/event-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     UsersComponent,
     EventsComponent,
-    FavoritesComponent
+    FormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
