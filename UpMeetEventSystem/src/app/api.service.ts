bdc675f76ad1727a0dca448048f2ca726db6975a
nikjk <nik.kuehn@outlook.com>
2023-06-07 20:07:16 -0400

    updated API Service with missing items.

--- UpMeetEventSystem/src/app/api.service.ts
+++ UpMeetEventSystem/src/app/api.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Favorites } from './favorites';
 import { Observable } from 'rxjs';
+import { Events } from './events';
+import { Users } from './users';
 
 
 @Injectable({
@@ -21,15 +23,26 @@ export class ApiService {
 
     return this.http.get(this.url + "Users");
    }
-
    
    getAllFavorites(id: number): Observable<Favorites>  {    
 
     return this.http.get<Favorites>(this.url + "Favorites/" + id );
-
-    
    }
 
+   getEvents(): Observable<Events[]> {
+    return this.http.get<Events[]>(`${this.url}/Events`);
+    }
 
+  getEvent(id: number): Observable<Events> {
+    return this.http.get<Events>(`${this.url}/GetEvent/${id}`);
+    }
+
+  createEvent(event: Events): Observable<Events> {
+    return this.http.post<Events>(`${this.url}/CreateEvent`, event);
+   }
+
+  deleteEvent(id: number): Observable<any> {
+    return this.http.delete<any>(`${this.url}/DeleteEvent/${id}`);
+  }
 
 }
